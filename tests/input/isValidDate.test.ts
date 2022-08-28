b2b6dcd2cc1aa257408afff6e06df8a999a7f7ba
m10rten <qmaartenenzo@gmail.com>
2022-08-28 16:42:29 +0200

    tests

--- tests/input/isValidDate.test.ts
+++ tests/input/isValidDate.test.ts
@@ -1,5 +1,17 @@
 import isValidDate from "../../src/lib/input/isValidDate";
 describe("isValidDate", () => {
+  it("should return true for '2020-01-01'", () => {
+    expect(isValidDate("2020-01-01")).toBe(true);
+  });
+  it("should return true for '2020-01-01T00:00:00.000Z'", () => {
+    expect(isValidDate("2020-01-01T00:00:00.000Z")).toBe(true);
+  });
+  it("should return true for new Date(1)", () => {
+    expect(isValidDate(new Date(1))).toBe(true);
+  });
+  it("should return false for NaN", () => {
+    expect(isValidDate(NaN)).toBe(false);
+  });
   it("should return true for 1", () => {
     expect(isValidDate(1)).toBe(true);
   });
@@ -30,5 +42,8 @@ describe("isValidDate", () => {
   it("should return false for classA", () => {
     class classA {}
     expect(isValidDate(classA)).toBe(false);
+  });
+  it("should return false for new Error", () => {
+    expect(isValidDate(new Error())).toBe(false);
   });
 });
